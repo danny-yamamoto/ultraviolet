2dae37be51d8ed5e9285d26f7f0ec6e0cda0c435
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-05-27 10:02:45 +0000

    Add

---
+++ apps/backend/src/todo/todo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class TodoService {
+  constructor(private prisma: PrismaService) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create(createTodoDto: CreateTodoDto) {
+    return 'This action adds a new todo';
+  }
+
+  findAll() {
+    return `This action returns all todo`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
